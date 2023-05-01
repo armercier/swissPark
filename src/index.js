9b5fde12ba6f7590dd252b4b9f5cbe844078d989
Arnaud Mercier <38730912+armercier@users.noreply.github.com>
2023-05-01 23:47:33 +0200

    added button and tried database reading

--- src/index.js
+++ src/index.js
@@ -5,6 +5,42 @@ import { Map, View } from "ol";
 import { Tile as TileLayer } from "ol/layer.js";
 import { XYZ } from "ol/source.js";
 import { defaults as defaultControls, ScaleLine } from "ol/control.js";
+import SQL from 'sql.js';
+
+const searchButton = document.getElementById("search-button");
+const dataButton = document.getElementById("data-button");
+
+
+searchButton.addEventListener("click", function() {
+  console.log("Search button clicked");
+});
+
+dataButton.addEventListener("click", function() {
+  console.log("data button clicked");
+  // const dbFile= 'mydatabase.db';
+
+  // fetch(dbFile)
+  //   .then(response => response.arrayBuffer())
+  //   .then(buffer => {
+  //     const db = new SQL.Database(new Uint8Array(buffer));
+  //     const results = db.exec('SELECT * FROM dbFile');
+  //     console.log(results);
+  //   })
+  //   .catch(error => {
+  //     console.error(error);
+  //   });
+
+  fetch('mydatabase.db')
+  .then(response => response.arrayBuffer())
+  .then(buffer => {
+    const db = new SQL.Database(new Uint8Array(buffer));
+    const results = db.exec('SELECT * FROM activities');
+    console.log(results);
+  });
+
+});
+
+
 
 const backgroundLayer = new TileLayer({
   id: "background-layer",
@@ -16,6 +52,7 @@ const backgroundLayer = new TileLayer({
 const view = new View({
   projection: "EPSG:3857",
   center: [900000, 5900000],
+  minZoom:8,
   zoom: 8
 });
 
@@ -29,3 +66,6 @@ new Map({
   layers: [backgroundLayer],
   view: view
 });
+
+
+
